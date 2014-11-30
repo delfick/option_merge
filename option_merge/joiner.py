05a9094230081aa89cf157834bd563ae6dd84a38
Stephen Moore <stephen@delfick.com>
2014-11-30 12:43:51 +1100

    Optimising dot_joiner a little

--- option_merge/joiner.py
+++ option_merge/joiner.py
@@ -6,26 +6,37 @@ import six
 
 def dot_joiner(*lsts):
     """Join lists of list of strings with a single dot in between each"""
-    return '.'.join(dot_joiner_path(lst) for lst in lsts)
+    if len(lsts) == 1:
+        return dot_joiner_path(lsts[0])
+    elif len(lsts) == 2:
+        return "{0}.{1}".format(dot_joiner_path(lsts[0]), dot_joiner_path(lsts[1]))
+    else:
+        return '.'.join(dot_joiner_path(lst) for lst in lsts)
 
 def dot_joiner_path(lst):
     """Join list of strings with a single dot in between each"""
     result = []
-    if isinstance(lst, six.string_types):
+    if hasattr(lst, "joined"):
+        return lst.joined()
+
+    if type(lst) in six.string_types:
         return lst
 
-    if not isinstance(lst, (list, tuple)):
+    if not type(lst) in (list, tuple):
         return unicode(lst)
 
     for part in lst:
-        if isinstance(part, (list, tuple)):
-            part = ''.join(part)
+        if hasattr(part, "joined"):
+            part = part.joined()
+        else:
+            if type(part) in (list, tuple):
+                part = ''.join(part)
 
-        while part and part.startswith("."):
-            part = part[1:]
+            while part and part.startswith("."):
+                part = part[1:]
 
-        while part and part.endswith("."):
-            part = part[:-1]
+            while part and part.endswith("."):
+                part = part[:-1]
 
         if part:
             result.append(part)
@@ -38,6 +49,11 @@ def join(one, two):
 
     Where either path is either string, list of strings or Path
     """
+    if not two:
+        return one
+    if not one:
+        return two
+
     from option_merge.path import Path
     if isinstance(one, Path):
         one = one.path

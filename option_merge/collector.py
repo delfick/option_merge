b09c5e7b44a18ac60dbf0b476f5ec772e1f6cd05
Stephen Moore <stephen@delfick.com>
2015-06-22 21:59:52 +1000

    Adding a collector shell

---
+++ option_merge/collector.py
@@ -0,0 +1,142 @@
+"""
+The collector object is responsible for collecting configuration
+and setting up converters
+"""
+
+from delfick_error import DelfickError
+from getpass import getpass
+import logging
+import os
+
+log = logging.getLogger("option_merge.collector")
+
+class Collector(object):
+    BadFileErrorKls = DelfickError
+    BadConfigurationErrorKls = DelfickError
+
+    def __init__(self, configuration_file):
+        self.configuration_file = configuration_file
+        self.configuration_folder = os.path.dirname(os.path.abspath(configuration_file))
+
+        self.configuration = self.collect_configuration(configuration_file)
+
+    ########################
+    ###   HOOKS
+    ########################
+
+    def alter_clone_cli_args(self, new_collector, new_cli_args, *args, **kwargs):
+        """Hook for altering cli_args given to a clone collector"""
+
+    def find_missing_config(self, configuration):
+        """Hook to raise errors about missing configuration"""
+
+    def extra_prepare(self, configuration, cli_args, available_tasks):
+        """Hook for any extra preparation before the converters are activated"""
+
+    def extra_prepare_after_activation(self, configuration, cli_args, available_tasks):
+        """Hook for any extra preparation after the converters are activated"""
+
+    def home_dir_configuration_location(self):
+        """Hook to return the location of the configuration in the user's home directory"""
+        return None
+
+    def read_file(self, location):
+        """Hook to read in a file and return a dictionary"""
+        raise NotImplementedError()
+
+    def start_configuration(self):
+        """Hook for starting the base of the configuration"""
+        raise NotImplementedError()
+
+    def add_configuration(self, configuration, collect_another_source, done, result, src):
+        """
+        Hook to add to the configuration the loaded result from src into configuration
+
+        The collect_another_source function can be used to collect another source
+
+        And done is a dictionary of configuration we have already added
+        """
+        raise NotImplementedError()
+
+    def extra_configuration_collection(self, configuration):
+        """Hook to do any extra configuration collection or converter registration"""
+
+    ########################
+    ###   USAGE
+    ########################
+
+    def clone(self, *args, **kwargs):
+        new_collector = self.__class__(self.configuration_file)
+        new_cli_args = dict(self.configuration["cli_args"].items())
+        self.alter_clone_cli_args(new_collector, new_cli_args, *args, **kwargs)
+        new_collector.prepare(new_cli_args)
+        return new_collector
+
+    def prepare(self, cli_args, available_tasks=None):
+        """Do the bespin stuff"""
+        self.find_missing_config(self.configuration)
+
+        self.configuration.update(
+            { "getpass": getpass
+            , "collector": self
+            , "cli_args": cli_args
+            , "config_root": self.configuration_folder
+            }
+        , source = "<preparation>"
+        )
+
+        self.extra_prepare(self.configuration, cli_args, available_tasks)
+        self.configuration.converters.activate()
+        self.extra_prepare_after_activation(self.configuration, cli_args, available_tasks)
+
+    ########################
+    ###   CONFIG
+    ########################
+
+    def collect_configuration(self, configuration_file):
+        """Return us a MergedOptions with this configuration and any collected configurations"""
+        errors = []
+
+        configuration = self.start_configuration()
+        configuration["config_root"] = self.configuration_folder
+
+        sources = [configuration_file]
+        home_dir_configuration = self.home_dir_configuration_location()
+        if home_dir_configuration:
+            sources.insert(0, home_dir_configuration)
+
+        done = set()
+        def add_configuration(src):
+            log.info("Adding configuration from %s", os.path.abspath(src))
+            if os.path.abspath(src) in done:
+                return
+            else:
+                done.add(os.path.abspath(src))
+
+            if src is None or not os.path.exists(src):
+                return
+
+            try:
+                if os.stat(src).st_size == 0:
+                    result = {}
+                else:
+                    result = self.read_file(src)
+            except self.BadFileErrorKls as error:
+                errors.append(error)
+                return
+
+            if not result:
+                return
+
+            self.add_configuration(configuration, add_configuration, done, result, src)
+
+        for source in sources:
+            add_configuration(source)
+
+        self.extra_configuration_collection(configuration)
+
+        if errors:
+            raise self.BadConfigurationErrorKls("Some of the configuration was broken", _errors=errors)
+
+        return configuration
+
---
+++ tests/test_collector.py
@@ -0,0 +1,301 @@
+# coding: spec
+
+from option_merge.collector import Collector
+from option_merge import MergedOptions
+
+from delfick_error import DelfickErrorTestMixin, DelfickError
+from noseOfYeti.tokeniser.support import noy_sup_setUp
+from contextlib import contextmanager
+from getpass import getpass
+import tempfile
+import unittest
+import shutil
+import mock
+import json
+import os
+
+class TestCase(unittest.TestCase, DelfickErrorTestMixin): pass
+
+describe TestCase, "Collector":
+    @contextmanager
+    def fake_config(self):
+        root = None
+        try:
+            root = tempfile.mkdtemp()
+            config_file = os.path.join(root, "config.json")
+            with open(config_file, 'w') as fle:
+                fle.write("\n{}")
+            yield (root, config_file)
+        finally:
+            if root and os.path.exists(root):
+                shutil.rmtree(root)
+
+    it "takes in configuration_file, determines configuration_folder and collects configuration":
+        with self.fake_config() as (config_root, config_file):
+            configuration = mock.Mock(name="configuration")
+            fake_collect_configuration = mock.Mock(name="collect_configuration")
+            fake_collect_configuration.return_value = configuration
+
+            with mock.patch.object(Collector, 'collect_configuration', fake_collect_configuration):
+                collector = Collector(config_file)
+                self.assertEqual(collector.configuration_file, config_file)
+                self.assertEqual(collector.configuration_folder, config_root)
+                self.assertIs(collector.configuration, configuration)
+
+            fake_collect_configuration.assert_called_once_with(config_file)
+
+    describe "Cloning":
+        it "returns an instance that has rerun collect_configuration and prepare":
+            called = []
+            original_cli_args = {'a': 1, "b": 2}
+            with self.fake_config() as (config_root, config_file):
+                class Col(Collector):
+                    def start_configuration(self): return MergedOptions.using({})
+                    def read_file(self, location): return json.load(open(location))
+                    def add_configuration(self, configuration, collect_another_source, done, result, src): configuration.update(result)
+
+                    def alter_clone_cli_args(slf, nw_cllctr, nw_cli_args, new_args):
+                        nw_cli_args.update(new_args)
+                        called.append((2, nw_cllctr, nw_cli_args))
+
+
+                collector = Col(config_file)
+                collector.prepare(original_cli_args)
+                self.assertEqual(collector.configuration["cli_args"].as_dict(), original_cli_args)
+
+            class MockCollectorKls(Col):
+                def __init__(self, config_file):
+                    called.append((1, config_file))
+
+                def prepare(slf, new_cli_args):
+                    called.append((3, new_cli_args))
+
+            collector.__class__ = MockCollectorKls
+
+            clone = collector.clone({"c": 3, "b": 4})
+            self.assertIs(type(clone), MockCollectorKls)
+            self.assertEqual(called, [(1, config_file), (2, clone, {"a": 1, "b": 4, "c": 3}), (3, {"a": 1, "b": 4, "c": 3})])
+            self.assertEqual(original_cli_args, {"a": 1, "b": 2})
+
+    describe "prepare":
+        it "find_missing_config, updates configuration, does extra_prepare, activates converters and extra_prepare_after_activation":
+            called = []
+            with self.fake_config() as (config_root, config_file):
+                class Col(Collector):
+                    def start_configuration(self): return MergedOptions.using({})
+                    def read_file(self, location): return json.load(open(location))
+                    def add_configuration(self, configuration, collect_another_source, done, result, src): configuration.update(result)
+
+                    def find_missing_config(slf, config):
+                        called.append((1, config))
+                        self.assertEqual(config.as_dict(), {"config_root": config_root})
+                        config.converters = mock.Mock(name="converters")
+
+                    def extra_prepare(slf, config, cli_args, available_tasks):
+                        called.append((2, config, cli_args))
+                        self.assertEqual(config.as_dict()
+                            , { "getpass": getpass
+                              , "collector": slf
+                              , "cli_args": cli_args
+                              , "config_root": config_root
+                              }
+                            )
+                        self.assertEqual(len(config.converters.mock_calls), 0)
+
+                    def extra_prepare_after_activation(slf, config, cli_args, available_tasks):
+                        called.append((3, config, cli_args))
+                        config.converters.activate.assert_called_once()
+
+                collector = Col(config_file)
+                self.assertEqual(called, [])
+
+                cli_args = mock.Mock(name="cli_args")
+                collector.prepare(cli_args)
+                self.assertEqual(called, [(1, collector.configuration), (2, collector.configuration, cli_args), (3, collector.configuration, cli_args)])
+
+    describe "Collecting configuration":
+        it "uses start_configuration, read_file, home_dir_configuration, config_file, add_configuration and extra_configuration_collection":
+            called = []
+            configuration = MergedOptions.using({})
+
+            result_home_dir = mock.Mock(name="result_home_dir")
+            result_config_file = mock.Mock(name="result_config_file")
+
+            with self.fake_config() as (config_root, config_file):
+                home_dir = os.path.join(config_root, 'home.json')
+                with open(home_dir, "w") as fle:
+                    fle.write("{}")
+                results = {config_file: result_config_file, home_dir: result_home_dir}
+
+                class Col(Collector):
+                    def start_configuration(slf):
+                        called.append((1, ))
+                        return configuration
+
+                    def read_file(slf, location):
+                        called.append((2, location))
+                        return results[location]
+
+                    def add_configuration(slf, config, collect_another_source, done, result, src):
+                        self.assertEqual(config["config_root"], config_root)
+                        called.append((3, config, result, src))
+
+                    def home_dir_configuration_location(slf): return home_dir
+
+                    def extra_configuration_collection(slf, config):
+                        self.assertEqual([c[0] for c in called], [1, 2, 3, 2, 3])
+                        called.append((4, config))
+
+                collector = Col(config_file)
+                self.assertIs(collector.configuration, configuration)
+                self.assertEqual(called
+                    , [ (1, )
+                      , (2, home_dir)
+                      , (3, configuration, result_home_dir, home_dir)
+                      , (2, config_file)
+                      , (3, configuration, result_config_file, config_file)
+                      , (4, configuration)
+                      ]
+                    )
+
+        it "ignores home_dir if it's not specified":
+            called = []
+            configuration = MergedOptions.using({})
+
+            result_config_file = mock.Mock(name="result_config_file")
+
+            with self.fake_config() as (config_root, config_file):
+                results = {config_file: result_config_file}
+
+                class Col(Collector):
+                    def start_configuration(slf):
+                        called.append((1, ))
+                        return configuration
+
+                    def read_file(slf, location):
+                        called.append((2, location))
+                        return results[location]
+
+                    def add_configuration(slf, config, collect_another_source, done, result, src):
+                        self.assertEqual(config["config_root"], config_root)
+                        called.append((3, config, result, src))
+
+                    def extra_configuration_collection(slf, config):
+                        self.assertEqual([c[0] for c in called], [1, 2, 3])
+                        called.append((4, config))
+
+                collector = Col(config_file)
+                self.assertIs(collector.configuration, configuration)
+                self.assertEqual(called
+                    , [ (1, )
+                      , (2, config_file)
+                      , (3, configuration, result_config_file, config_file)
+                      , (4, configuration)
+                      ]
+                    )
+
+        it "gives a function for adding more sources to add_configuration":
+            called = []
+            configuration = MergedOptions.using({})
+
+            with self.fake_config() as (config_root, config_file):
+                other_loc = os.path.join(config_root, 'other.json')
+                with open(other_loc, "w") as fle:
+                    fle.write("{}")
+                results = {config_file: {"extra": other_loc}, other_loc: {"stuff": "a"}}
+
+                class Col(Collector):
+                    def start_configuration(slf):
+                        called.append((1, ))
+                        return configuration
+
+                    def read_file(slf, location):
+                        called.append((2, location))
+                        return results[location]
+
+                    def add_configuration(slf, config, collect_another_source, done, result, src):
+                        self.assertEqual(config["config_root"], config_root)
+                        if "extra" in result:
+                            collect_another_source(result["extra"])
+                        called.append((3, config, result, src))
+
+                    def extra_configuration_collection(slf, config):
+                        self.assertEqual([c[0] for c in called], [1, 2, 2, 3, 3])
+                        called.append((4, config))
+
+                collector = Col(config_file)
+                self.assertIs(collector.configuration, configuration)
+                self.assertEqual(called
+                    , [ (1, )
+                      , (2, config_file)
+                      , (2, other_loc)
+                      , (3, configuration, {"stuff": "a"}, other_loc)
+                      , (3, configuration, {"extra": other_loc}, config_file)
+                      , (4, configuration)
+                      ]
+                    )
+
+        it "Can't create a circular loop using collect_another_source":
+            called = []
+            configuration = MergedOptions.using({})
+
+            with self.fake_config() as (config_root, config_file):
+                other_loc = os.path.join(config_root, 'other.json')
+                with open(other_loc, "w") as fle:
+                    fle.write("{}")
+                results = {config_file: {"extra": other_loc}, other_loc: {"extra": config_file}}
+
+                class Col(Collector):
+                    def start_configuration(slf):
+                        called.append((1, ))
+                        return configuration
+
+                    def read_file(slf, location):
+                        called.append((2, location))
+                        return results[location]
+
+                    def add_configuration(slf, config, collect_another_source, done, result, src):
+                        self.assertEqual(config["config_root"], config_root)
+                        collect_another_source(result["extra"])
+                        called.append((3, config, result, src))
+
+                    def extra_configuration_collection(slf, config):
+                        self.assertEqual([c[0] for c in called], [1, 2, 2, 3, 3])
+                        called.append((4, config))
+
+                collector = Col(config_file)
+                self.assertIs(collector.configuration, configuration)
+                self.assertEqual(called
+                    , [ (1, )
+                      , (2, config_file)
+                      , (2, other_loc)
+                      , (3, configuration, {"extra": config_file}, other_loc)
+                      , (3, configuration, {"extra": other_loc}, config_file)
+                      , (4, configuration)
+                      ]
+                    )
+
+        it "collects errors from reading files and raises a mother exception":
+            class BadJson(DelfickError): pass
+            class BadConfiguration(DelfickError): pass
+
+            called = []
+            with self.fake_config() as (config_root, config_file):
+                home_dir = os.path.join(config_root, 'home.json')
+                with open(home_dir, "w") as fle:
+                    fle.write("{}")
+
+                class Col(Collector):
+                    BadFileErrorKls = BadJson
+                    BadConfigurationErrorKls = BadConfiguration
+
+                    def start_configuration(slf): called.append(0); return MergedOptions.using({})
+                    def read_file(slf, location): called.append((1, location)); raise BadJson(location=location)
+                    def add_configuration(slf, *args, **kwargs): assert False, "This shouldn't get called"
+                    def extra_configuration_collection(slf, config): called.append(2)
+                    def home_dir_configuration_location(slf): return home_dir
+
+                with self.fuzzyAssertRaisesError(BadConfiguration, "Some of the configuration was broken", _errors=[BadJson(location=home_dir), BadJson(location=config_file)]):
+                    collector = Col(config_file)
+                self.assertEqual(called, [0, (1, home_dir), (1, config_file), 2])
+

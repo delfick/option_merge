6c5c93bc9b3680582df20fb1de37837155146fc0
Stephen Moore <stephen@delfick.com>
2016-06-05 18:19:26 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name = "option_merge"
-    , version = "1.4"
+    , version = "1.4.1"
     , packages = ['option_merge'] + ['option_merge.%s' % pkg for pkg in find_packages('option_merge')]
     , include_package_data = True
 
